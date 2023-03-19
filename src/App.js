e6db8c9b0d57358392746af5b70ccd7daec0bb9f
Camilo <crojas.historia@gmail.com>
2023-03-18 20:38:19 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Login from "./components/Login";
+
+function App() {
+
+  const [alert, setAlert] =useState ({
+    mensaje: "",
+    color: ""
+  });
+
+  return (
+    <>
+      <Login setAlert={setAlert}/>
+      
+      {alert.mensaje !== ""? <Alert color={alert.color}>{alert.mensaje}</Alert> : null}
+    </>
+  );
+}
+
+export default App;
